12c02703064d9e99e390f7f3a3d0be97a13a619f
MastaWu <wu.stephend@gmail.com>
2015-04-06 11:55:33 -0400

    Error was in route.js, I used 'next' instead of 'next()'....

--- apps/routes.js
+++ apps/routes.js
@@ -70,36 +70,58 @@ module.exports = function(app, passport){
 
     });*/
 
-    app.get('/', function(req, res){
-
-        res.render('index.ejs');
-
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
     });
 
-    app.get('/login', function(req, res){
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
 
-        res.render('login.ejs', {message: req.flash('loginMessage')});
-
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
-    app.get('/signup', function(req, res){
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
 
-        res.render('signup.ejs', {message: req.flash('signupMessage')});
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
 
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
 
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
 
-    app.get('logout', function(req, res){
-
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
-
     });
+
 
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
@@ -116,13 +138,13 @@ module.exports = function(app, passport){
 
     }));
 
-}
+};
 
 function isLoggedIn(req, res, next){
 
     if(req.isAuthenticated()){
 
-        return next;
+        return next();
 
     } else{
 
